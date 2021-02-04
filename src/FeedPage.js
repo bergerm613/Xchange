fa2340f2dfc5fcb7ec4e407b506b4dd33aa3ffc9
Devora Nagel <devoranagel@gmail.com>
2021-02-04 11:21:19 -0600

    added feedItem and quote

--- src/FeedPage.js
+++ src/FeedPage.js
@@ -1,10 +1,30 @@
 import './App.css'
 import React, { Component } from "react"
+import FeedItem from './FeedItem'
 
 class FeedPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      items:[{itemName: "Cute Top", description: "This is a description of a cute top", date: "02/04/2021"}]
+    }
+  }
   render() {
     return (
-      <div>This is the feed</div>
+      <div className="feed_page">
+        <div className="feed_quote">Insprational Quote Goes Here</div>
+        {
+          this.state.items.map((item, index) => {
+            return (
+            <FeedItem 
+              itemName={item.itemName} 
+              description={item.description}
+              date={item.date}
+            />
+            )
+          })
+        }
+      </div>
     )
   }
 }
