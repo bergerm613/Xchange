79ecb262779172ed82104428264c283ac9b11563
Devora Nagel <devoranagel@gmail.com>
2021-02-12 11:43:21 -0600

    added image file upload and paragraph

--- src/NewPostPage.js
+++ src/NewPostPage.js
@@ -6,10 +6,18 @@ class NewPostPage extends Component {
     super(props);
     this.state = {
       title: '',
+      imageFile: '',
       description: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    const hiddenFileInput = React.createRef();
+  }
+
+  fileUploaded(event) {
+    const file = event.target.files[0];
+    this.setState({ imageFile: file });
   }
 
   handleSubmit(event) {
@@ -22,26 +30,43 @@ class NewPostPage extends Component {
       <div>
         <div className="spartan list_an_item">List an Item</div>
 
-        <form className="form_container" onSubmit={this.handleSubmit}>
-          <input
-            className="input_title"
-            placeholder="Title"
-            type="text"
-            required="true"
-            value={this.state.title}
-            onChange={event => this.setState({ title: event.target.value })} />
+        <div className="form_and_par flex">
+          <form className="form_container" onSubmit={this.handleSubmit}>
+            <input
+              className="input_title"
+              placeholder="Title"
+              type="text"
+              required="true"
+              value={this.state.title}
+              onChange={event => this.setState({ title: event.target.value })} />
 
-          <textarea
-            className="description_text_area"
-            rows="10"
-            cols="30"
-            required="true"
-            placeholder="Description"
-            value={this.state.description}
-            onChange={event => this.setState({ description: event.target.value })} />
+            <button onClick={this.fileUploaded}>Upload Image File</button>
 
-          <input className="submit_btn" type="submit" value="Submit" />
-        </form>
+            <input
+              style={{ display: 'none' }}
+              type="file"
+              accept="image/png, image/jpeg"
+              ref={this.hiddenFileInput} />
+
+            <textarea
+              className="description_text_area"
+              rows="10"
+              cols="30"
+              required="true"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={event => this.setState({ description: event.target.value })} />
+
+            <input className="submit_btn spartan size_21" type="submit" value="Submit" />
+          </form>
+          <div className="txt_paragraph spartan size_21">
+            Hey there! <br /> <br />
+            We’ll probably want some text here explaining the process of posting a new listing,
+            but for now, this will do. <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+        </div>
       </div>
     )
   }
